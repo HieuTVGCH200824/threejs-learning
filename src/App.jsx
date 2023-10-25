247c9c89c485f4b1015b0269e31386a70503290a
HieuTVGCH200824 <hieutvgch200824@fpt.edu.vn>
2023-10-25 15:24:07 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Suspense, useRef } from "react";
+import "./App.css";
+
+const Model = () => {
+  const gltf = useLoader(GLTFLoader, "./potiontest.glb");
+  const modelRef = useRef();
+
+  useFrame(({ clock }) => {
+    // Rotate the model around the Y-axis
+    modelRef.current.rotation.y = Math.sin(clock.elapsedTime) * 0.5;
+  });
+  return (
+    <>
+      <primitive object={gltf.scene} ref={modelRef} scale={1} />
+    </>
+  );
+};
+
+export default function App() {
+  return (
+    <div className="h-screen w-screen">
+      <Canvas>
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
